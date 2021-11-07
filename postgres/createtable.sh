40ba57f03b8ff7d25773f3750bdb0535377fc4aa
Anson Kee <anson.kee@jobvite-inc.com>
2021-11-07 10:20:24 -0800

    createtable.sh idempotent

--- postgres/createtable.sh
+++ postgres/createtable.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x
 
-if [[ $(psql --host ${DB_HOST} -U postgres -Atql | grep ${DB_NAME}) ]]; then 
+if [[ $(psql --host ${DB_HOST} -U postgres -d ${DB_NAME} -c '\dt' | grep ${DB_NAME}) ]]; then 
+#psql --host localhost -U postgres  -p 5432 -d todo -c '\dt' |grep Todo
 #if [[ -z $(psql --host ${DB_HOST} -U postgres -Atql | grep ${DB_NAME}) ]]; then 
   yarn create-tables
 else 
